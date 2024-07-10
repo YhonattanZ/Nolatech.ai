a6589818b7e413272233b05b8c5514c0b84b3179
unknown <yonattanz11@gmail.com>
2024-07-10 09:35:43 -0400

    feat(Login) basic structure + constants

--- prueba_nolatech/lib/main.dart
+++ prueba_nolatech/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
+import 'src/providers/login_provider.dart';
 import 'src/view/screens/login.dart';
 
 void main() {
@@ -13,12 +14,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MultiProvider(
-      providers: const [],
+      providers: [
+        ChangeNotifierProvider(create: (context) => LoginProvider()),
+      ],
       child: MaterialApp(
-        localizationsDelegates: const [],
-        supportedLocales: const [
-          Locale('es'),
-        ],
         debugShowCheckedModeBanner: false,
         title: 'Prueba Nolatech',
         theme: ThemeData(primarySwatch: Colors.blue),
---
+++ prueba_nolatech/lib/src/constants/constants.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+const Color primaryColor = Color(0xffB3D92B);
+const Color secondaryColor = Color(0xff365902);
+const Color fontColor = Color(0xff591E2E);
