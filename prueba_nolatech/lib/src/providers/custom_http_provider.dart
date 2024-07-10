47d320408e8c07a0563e55988ff14e8f8b62d28d
unknown <yonattanz11@gmail.com>
2024-07-10 10:38:37 -0400

    feat(CustomHttpProvider)request get, post, put and delete

--- prueba_nolatech/lib/src/providers/custom_http_provider.dart
+++ prueba_nolatech/lib/src/providers/custom_http_provider.dart
@@ -1,3 +1,52 @@
-import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 
-class CustomHttpProvider extends ChangeNotifier {}
+class CustomHttpProvider {
+  final String baseUrl;
+
+  CustomHttpProvider({required this.baseUrl});
+
+  Future<http.Response> get(String endpoint) async {
+    final response = await http.get(Uri.parse('$baseUrl$endpoint'));
+    _logStatusCode(response);
+    return response;
+  }
+
+  Future<http.Response> post(String endpoint,
+      {Map<String, dynamic>? body}) async {
+    final response = await http.post(
+      Uri.parse('$baseUrl$endpoint'),
+      headers: {'Content-Type': 'application/json'},
+      body: json.encode(body),
+    );
+    _logStatusCode(response);
+    return response;
+  }
+
+  Future<http.Response> put(String endpoint,
+      {Map<String, dynamic>? body}) async {
+    final response = await http.put(
+      Uri.parse('$baseUrl$endpoint'),
+      headers: {'Content-Type': 'application/json'},
+      body: json.encode(body),
+    );
+    _logStatusCode(response);
+    return response;
+  }
+
+  Future<http.Response> delete(String endpoint) async {
+    final response = await http.delete(Uri.parse('$baseUrl$endpoint'));
+    _logStatusCode(response);
+    return response;
+  }
+
+  void _logStatusCode(http.Response response) {
+    if (response.statusCode == 200) {
+      print(
+          'Request to ${response.request?.url} was successful with status code: ${response.statusCode}');
+    } else {
+      print(
+          'Request to ${response.request?.url} failed with status code: ${response.statusCode}');
+    }
+  }
+}
