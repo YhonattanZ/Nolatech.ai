0f6492cbaf128fc0b5c2364162f8d265006ffa98
unknown <yonattanz11@gmail.com>
2024-07-11 11:49:11 -0400

    style(CourtInfoPage) last changes in design

--- prueba_nolatech/lib/src/view/screens/court_info_page.dart
+++ prueba_nolatech/lib/src/view/screens/court_info_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_image_slideshow/flutter_image_slideshow.dart';
+import 'package:intl/intl.dart';
 import 'package:like_button/like_button.dart';
 import 'package:provider/provider.dart';
 import 'package:prueba_nolatech/src/constants/constants.dart';
@@ -15,7 +16,10 @@ class CourtInfo extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Stack(
               children: [
@@ -23,10 +27,143 @@ class CourtInfo extends StatelessWidget {
                 courtHeaderButtons(context),
               ],
             ),
-            infoCourt()
+            infoCourt(),
+            scheduleCourt(context)
           ],
         ),
       ),
+    );
+  }
+
+  Widget scheduleCourt(context) {
+    return Padding(
+      padding: const EdgeInsets.all(20.0),
+      child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: const Text(
+                'Fecha y Hora',
+                style: TextStyle(
+                    fontSize: fontSize * 1.2,
+                    fontWeight: FontWeight.bold,
+                    color: primaryColor),
+              ),
+            ),
+            Consumer<CourtsProvider>(
+              builder: (_, p, i) => dateSelector(() {
+                p.showDate(context);
+              }, 'Fecha:', p.date),
+            ),
+            Consumer<CourtsProvider>(
+              builder: (_, p, i) => timeSelector(() {
+                p.showInitHour(context);
+              }, 'Hora de inicio:', p.initHour),
+            ),
+            Consumer<CourtsProvider>(
+              builder: (_, p, i) => timeSelector(() {
+                p.showEndHour(context);
+              }, 'Hora de fin:', p.endHour),
+            ),
+            const Padding(
+              padding: EdgeInsets.only(top: 20.0, left: 8),
+              child: Text(
+                'Agrega un comentario',
+                style: TextStyle(
+                    fontSize: fontSize * 1.2,
+                    fontWeight: FontWeight.bold,
+                    color: primaryColor),
+              ),
+            ),
+            addCommentsBox(context),
+            Container(
+                height: 50,
+                margin:
+                    const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                width: double.infinity,
+                child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                        shape: const RoundedRectangleBorder(
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(10))),
+                        backgroundColor: fontColor),
+                    onPressed: () {},
+                    child: const Text(
+                      'Reservar',
+                      style: TextStyle(
+                          color: Colors.white, fontSize: fontSize * 1.2),
+                    )))
+          ]),
+    );
+  }
+
+  Widget addCommentsBox(context) {
+    final provider = Provider.of<CourtsProvider>(context, listen: false);
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 20),
+      child: TextField(
+        textInputAction: TextInputAction.done,
+        controller: provider.commentsCtrl,
+        style: const TextStyle(color: Colors.blue),
+        scrollPadding:
+            EdgeInsets.only(bottom: MediaQuery.of(context).viewInsets.bottom),
+        maxLines: 3,
+        textAlign: TextAlign.start,
+        decoration: const InputDecoration(
+          hintText: 'Agrega un comentario...',
+          hintStyle: TextStyle(
+            color: Colors.grey,
+            fontSize: 16,
+          ),
+          disabledBorder: OutlineInputBorder(
+              borderSide: BorderSide(width: 0.5, color: Colors.grey)),
+          focusedBorder:
+              OutlineInputBorder(borderSide: BorderSide(color: primaryColor)),
+          enabledBorder:
+              OutlineInputBorder(borderSide: BorderSide(color: Colors.grey)),
+          border: OutlineInputBorder(borderSide: BorderSide(width: 0.2)),
+        ),
+      ),
+    );
+  }
+
+  Widget dateSelector(VoidCallback onTap, String title, DateTime date) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+          padding: const EdgeInsets.symmetric(horizontal: 15),
+          margin: const EdgeInsets.fromLTRB(20, 20, 20, 0),
+          height: 50,
+          decoration: BoxDecoration(
+              color: Colors.white, borderRadius: BorderRadius.circular(10)),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(title, style: const TextStyle(fontSize: 16)),
+              Text(DateFormat('dd/MM/yyyy').format(date)),
+            ],
+          )),
+    );
+  }
+
+  Widget timeSelector(VoidCallback onTap, String title, DateTime? date) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+          padding: const EdgeInsets.symmetric(horizontal: 15),
+          margin: const EdgeInsets.fromLTRB(20, 20, 20, 0),
+          height: 50,
+          decoration: BoxDecoration(
+              color: Colors.white, borderRadius: BorderRadius.circular(10)),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(title, style: const TextStyle(fontSize: 16)),
+              Text(DateFormat('hh:mm').format(date ?? DateTime.now())),
+            ],
+          )),
     );
   }
 
